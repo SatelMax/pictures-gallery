4ba617568257de5cb4b6fb96884954a63649b60f
Sergey <satellite.jkt@gmail.com>
2024-02-16 06:01:03 +0700

    added request to the unsplash api

--- frontend/src/components/Welcome.js
+++ frontend/src/components/Welcome.js
@@ -3,7 +3,7 @@ import { Jumbotron, Button } from "react-bootstrap";
 
 const Welcome = () => (
   <Jumbotron>
-    <h1>Pictures Gallery</h1>
+    <h1>Welcome to Pictures Gallery</h1>
     <p>
       This is a simple application that retrieves photos using Unsplash API. In
       order to start enter any search term in the input fields.
